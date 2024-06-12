46791a7599fc0060846a904717612ade607f9f06
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-06-12 14:05:24 +0100

    complete WikiSearch project

--- projects/WikiSearch/App/views.py
+++ projects/WikiSearch/App/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse 
+import wikipedia 
 
-# Create your views here.
+
+# Create your views here. 
+def home(request): 
+	if request.method == "POST": 
+		search = request.POST['search'] 
+		try: 
+			result = wikipedia.summary(search,sentences = 3) #No of sentences that you want as output 
+		except: 
+			return HttpResponse("Wrong Input") 
+		return render(request, "index.html", {"result":result}) 
+	return render(request, "index.html") 
