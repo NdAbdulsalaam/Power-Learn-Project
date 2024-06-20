ee1e24c2a5d6ee1dcabc5fa6b0bba44d4bb699c0
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-06-20 05:39:38 +0100

    add urls

---
+++ projects/RxLocator/App/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.pharmacy_list, name='pharmacy_list'),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('pharmacy/<int:pharmacy_id>/', views.pharmacy_detail, name='pharmacy_detail'),
+    path('pharmacy/<int:pharmacy_id>/add_review/', views.add_review, name='add_review'),
+]
