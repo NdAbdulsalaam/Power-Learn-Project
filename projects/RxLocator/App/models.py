b2d947f76f493d6395037c51da4586cf1b96a9d3
NdAbdulsalaam <olaitansalaam2012@gmail.com>
2024-06-20 05:32:06 +0100

    create models

--- projects/RxLocator/App/models.py
+++ projects/RxLocator/App/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Pharmacy(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=250)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=10)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    open_24_hours = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Review(models.Model):
+    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.rating} by {self.user} for {self.pharmacy}'
